6421bf1b64b6ecdad1a7e426ea1aeffb50a25a98
MohammedAnasK <anaskaluji@gmail.com>
2024-03-02 17:32:39 +0530

    created components

---
+++ src/component/Work.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Work() {
+  return (
+    <div>
+      <div
+        id="work"
+        className="w-full h-screen bg-slate-600 text-white text-center grid gap-4 grid-cols-5  p-4 overflow-x-scroll  max-md:grid-cols-1  "
+      >
+        <div className=" h-96  bg-orange-500 rounded-lg ">
+          {/* <img
+        className="h-auto"
+        src="pexels-pixabay-36762.jpg"
+        alt="image 1"
+      /> */}
+        </div>
+        <div className="h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-45853.jpg" alt="image 1" /> */}
+        </div>
+        <div className="h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-36762.jpg" alt="image 1" /> */}
+        </div>
+        <div className=" h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-36762.jpg" alt="image 1" /> */}
+        </div>
+        <div className="h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-36762.jpg" alt="image 1" /> */}
+        </div>
+        <div className="h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-36762.jpg" alt="image 1" /> */}
+        </div>
+        <div className="h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-36762.jpg" alt="image 1" /> */}
+        </div>
+        <div className="h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-36762.jpg" alt="image 1" /> */}
+        </div>
+        <div className="h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-36762.jpg" alt="image 1" /> */}
+        </div>
+        <div className="h-96 bg-orange-500 rounded-lg">
+          {/* <img className="" src="pexels-pixabay-36762.jpg" alt="image 1" /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Work;
