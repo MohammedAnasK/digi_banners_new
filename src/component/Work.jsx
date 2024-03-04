7725d4041d923c560ba2ebc29b315ac575d21698
jenish <panditjenish5@gmail.com>
2024-03-04 11:22:03 +0530

    chnges

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const Work = () => {
-  return (
-    <div>
-       
-      <div id="work" className="w-full h-screen bg-slate-600 text-white text-center grid gap-4 grid-cols-5 grid-rows-2 p-4 max-md:grid-cols-2 ">
-          
-          <div className="grid bg-orange-500 rounded-lg ">
-            <img className="h-auto" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div> 
-          <div className="grid bg-orange-500 rounded-lg">
-          <img className="" src="pexels-pixabay-45853.jpg" alt="image 1"/>
-          </div>
-          <div className="grid bg-orange-500 rounded-lg">
-          <img className="" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div>
-          <div className="grid bg-orange-500 rounded-lg">
-          <img className="" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div>
-          <div className="grid bg-orange-500 rounded-lg">
-          <img className="" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div>
-          <div className="grid bg-orange-500 rounded-lg">
-          <img className="" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div>
-          <div className="grid bg-orange-500 rounded-lg">
-          <img className="" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div>
-          <div className="grid bg-orange-500 rounded-lg">
-          <img className="" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div>
-          <div className="grid bg-orange-500 rounded-lg">
-          <img className="" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div>
-          <div className="grid bg-orange-500 rounded-lg">
-            <img className="" src="pexels-pixabay-36762.jpg" alt="image 1"/>
-          </div>
-        </div>
-    </div>
-  )
-}
-
-export default Work
-=======
 import React from "react";
 
 function Work() {
@@ -93,4 +47,3 @@ function Work() {
 }
 
 export default Work;
->>>>>>> 6421bf1b64b6ecdad1a7e426ea1aeffb50a25a98
